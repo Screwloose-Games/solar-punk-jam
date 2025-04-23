2b5f42179afc2073872e4102e2abe710906a8496
Josep Valls <josep@valls.name>
2025-04-22 18:24:54 -0700

    toolbar into hud

--- test/scenarios/rooftops/rooftops_v2.gd
+++ test/scenarios/rooftops/rooftops_v2.gd
@@ -35,13 +35,19 @@ func move_cursor_3d():
 		$SelectionPlaceholder.position = intersect
 		$StructurePlaceholder.position = intersect + Vector3(-0.5,0,-0.5)
 
+@onready var hud := $HUDCanvasLayer as HUDCanvasLayer
 func load_building_palette():
 	cached_world_data = get_world_data()
+	hud.cached_world_data = cached_world_data
 	for idx in len(cached_world_data):
-		var b = Button.new()
-		b.connect("pressed", select_structure_from_palette.bind(idx))
-		b.text = cached_world_data[idx][0]
-		$CanvasLayer/VBoxContainer/VBoxContainer.add_child(b)
+		if false:
+			var b = Button.new()
+			b.connect("pressed", select_structure_from_palette.bind(idx))
+			b.text = cached_world_data[idx][0]
+			$CanvasLayer/VBoxContainer/VBoxContainer.add_child(b)
+		else:
+			hud.register_structure(idx)
+	hud.connect("BuildableStructureSelected", self.select_structure_from_palette)
 
 func build_structure():
 	var structure := $Structure.duplicate() as MeshInstance3D
@@ -64,30 +70,30 @@ func select_structure_from_world():
 	pass
 
 static func get_world_data():
-	# name,priority,height,width,depth,description,filename
-	var data_ = """Workshop,,2.8,4,4,Building for tool use - repair - fabrication,workshop.tscn
-Wind turbine/Kite,Med,12,3,3,Tall structure or airborne device generating power from wind,wind_turbine_or_kite.tscn
-Water tank,High,3,3,3,Large container for storing clean water,water_tank.tscn
-Wastewater treatment,,3,4,4,Multi-tank system for filtering and purifying greywater,wastewater_treatment.tscn
-Vertical Hydroponics,,2.5,2,1,Tower structure for soil-less vertical farming,vertical_hydroponics.tscn
-Vegetable garden,High,0.5,3,2,Small garden bed for growing vegetables,vegetable_garden.tscn
-Tent/Yurt/Chill area,High,2.5,5,5,Communal shaded or sheltered resting space,tent_yurt_chill_area.tscn
-Solar panel,High,1.2,2,1,Photovoltaic panel for converting sunlight into electricity,solar_panel.tscn
-Recycling station,High,1.8,2,2,Bins and sorting area for recyclable materials,recycling_station.tscn
-Rain collection,High,2,2,2,Structure for harvesting rainwater - often elevated,rain_collection.tscn
-Pantry/Storage,,2,3,2,Storage shed for food or supplies,pantry_storage.tscn
-Kitchen,High,2.5,4,3,Communal cooking space with equipment,kitchen.tscn
-Hygiene station,,2,2,2,Outdoor sink/shower area for personal hygiene,hygiene_station.tscn
-Garbage dump,,1,3,3,Area for non-recyclable waste,garbage_dump.tscn
-Fruit bushes/trees,Med,2.5,3,3,Fruit-bearing trees or bushes,fruit_bushes_trees.tscn
-Fermentation,,1.8,1,1,Small structure with sealed containers for fermentation,fermentation.tscn
-Exercise station,,2.5,3,3,Outdoor space with equipment for physical activity,exercise_station.tscn
-Composting and regenerative agriculture,,1,2,2,Compost bin or area with regenerative farming tools,composting_regen_agriculture.tscn
-Bioreactor,,2.5,2,2,Tank system for converting organic matter into energy or fuel,bioreactor.tscn
-Battery,High,1.5,1,1,Power storage unit,battery.tscn
-Atmospheric water harvesting,,2.5,2,2,Device that condenses and collects water from the air,atmospheric_water_harvesting.tscn
-Arts and crafts station,,2,3,3,Covered station with tools for creative work,arts_crafts_station.tscn
-Aquaponics,,1.5,3,2,Combined fish and plant growing system,aquaponics.tscn"""
+	# Name,priority,height,width,depth,description,model filename,Icon,Art Status,Electricity Generation/Use,Water Generation/Use,Food Generation/Use,Happiness Generation,Electricity Stroage,Water Storage,Food Storage,Cost
+	var data_ = """Workshop,,2.8,4,4,Building for tool use - repair - fabrication,workshop.tscn,4/0,Not started,,,,,,,,
+Wind turbine/Kite,Med,12,3,3,Tall structure or airborne device generating power from wind,wind_turbine_or_kite.tscn,2/0,Not started,,,,,,,,
+Water tank,High,3,3,3,Large container for storing clean water,water_tank.tscn,0/2,Not started,,,,,,100,,
+Wastewater treatment,,3,4,4,Multi-tank system for filtering and purifying greywater,wastewater_treatment.tscn,0/2,Not started,,,,,,,,
+Vertical Hydroponics,,2.5,2,1,Tower structure for soil-less vertical farming,vertical_hydroponics.tscn,0/3,Not started,,,,,,,,
+Vegetable garden,High,0.5,3,2,Small garden bed for growing vegetables,vegetable_garden.tscn,0/0,Concept,,,,,,,,
+Tent/Yurt/Chill area,High,2.5,5,5,Communal shaded or sheltered resting space,tent_yurt_chill_area.tscn,0/4,Not started,,,,,,,,
+Solar panel,High,1.2,2,1,Photovoltaic panel for converting sunlight into electricity,solar_panel.tscn,1/0,Concept,,,,,,,,
+Recycling station,High,1.8,2,2,Bins and sorting area for recyclable materials,recycling_station.tscn,0/4,Not started,,,,,,,,
+Rain collection,High,2,2,2,Structure for harvesting rainwater - often elevated,rain_collection.tscn,0/2,Concept,,,,,,,,
+Pantry/Storage,,2,3,2,Storage shed for food or supplies,pantry_storage.tscn,0/4,Not started,,,,,,,100,
+Kitchen,High,2.5,4,3,Communal cooking space with equipment,kitchen.tscn,0/4,Not started,,,,,,,,
+Hygiene station,,2,2,2,Outdoor sink/shower area for personal hygiene,hygiene_station.tscn,0/4,Not started,,,,,,,,
+Garbage dump,,1,3,3,Area for non-recyclable waste,garbage_dump.tscn,0/4,Not started,,,,,,,,
+Fruit bushes/trees,Med,2.5,3,3,Fruit-bearing trees or bushes,fruit_bushes_trees.tscn,0/0,Not started,,,,,,,,
+Fermentation,,1.8,1,1,Small structure with sealed containers for fermentation,fermentation.tscn,0/3,Not started,,,,,,,,
+Exercise station,,2.5,3,3,Outdoor space with equipment for physical activity,exercise_station.tscn,0/4,Not started,,,,,,,,
+Compost,,1,2,2,Compost bin or area with regenerative farming tools,composting_regen_agriculture.tscn,3/0,Concept,,,,,,,,
+Bioreactor,,2.5,2,2,Tank system for converting organic matter into energy or fuel,bioreactor.tscn,0/1,Concept,,,,,,,,
+Battery,High,1.5,1,1,Power storage unit,battery.tscn,0/1,Not started,,,,,100,,,
+Atmospheric water harvesting,,2.5,2,2,Device that condenses and collects water from the air,atmospheric_water_harvesting.tscn,0/2,Not started,,,,,,,,
+Arts and crafts station,,2,3,3,Covered station with tools for creative work,arts_crafts_station.tscn,0/4,Not started,,,,,,,,
+Aquaponics,,1.5,3,2,Combined fish and plant growing system,aquaponics.tscn,0/3,Not started,,,,,,,,"""
 	var data = data_.split("\n")
 	data = Array(data)
 	for i in len(data):
--- test/scenarios/test_community_metrics_ui/ui_canvas_layer.gd
+++ test/scenarios/test_community_metrics_ui/ui_canvas_layer.gd
@@ -1,5 +1,40 @@
 extends CanvasLayer
+class_name HUDCanvasLayer
+
+signal BuildableStructureSelected(idx: int)
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ToggleUi"):
 		visible = !visible
+
+
+class BuildableStructure:
+	var idx: int
+	var ui: Control
+	func _init(idx, ui) -> void:
+		self.idx = idx
+		self.ui = ui
+
+@onready var buildable_structure_ui_template = $HUD/BottomCenterMarginContainer/ToolbarBackgroundPanelContainer/ToolbarMarginContainer/ToolbarHBoxContainer/ToolbarItemPanelContainer
+var buildable_structures: Array[BuildableStructure] = []
+
+# TODO make world_data available as singleton or autoload
+var cached_world_data = []
+		
+func register_structure(idx):
+	var ui = buildable_structure_ui_template.duplicate()
+	buildable_structure_ui_template.get_parent().add_child(ui)
+	buildable_structures.append(BuildableStructure.new(idx, ui))
+	var icon = ui.get_node("TextureRect") as TextureRect
+	icon.connect("gui_input", handle_gui_input.bind(idx))
+	icon.texture = icon.texture.duplicate()
+	var atlas = icon.texture as AtlasTexture
+	var icon_atlas_coords = cached_world_data[idx][7].split("/")
+	atlas.region = Rect2(80*int(icon_atlas_coords[0]),80*int(icon_atlas_coords[1]),80,80)
+	ui.show()
+	
+
+func handle_gui_input(event: InputEvent, idx: int):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		BuildableStructureSelected.emit(idx)
+	
