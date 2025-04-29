e8bd263b44a4654abfb1d77c6b80b362a7f2f6be
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2025-04-29 11:31:54 -0700

    Add scrap pile / spawner

---
+++ components/collectable.gd
@@ -0,0 +1,20 @@
+class_name Collectable
+extends Node3D
+
+signal collected
+
+#@export var collected_resource: ResourceTracker.ResourceType
+@export var collected_resource: String
+@export var count: int
+
+@onready var interactable_area_3d: InteractableArea3D = %InteractableArea3D
+
+func _ready():
+	interactable_area_3d.interacted.connect(_on_interacted)
+
+func _on_interacted(_player: Player):
+	collect()
+
+func collect():
+	EnvironmentManager.gain_resource(collected_resource, count)
+	collected.emit()
