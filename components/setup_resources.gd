b2a4c9c7520ff9a344e36f6e0b4fc9ea3faad1eb
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2025-04-29 12:30:37 -0700

    Make buildable area in buildable scene

---
+++ components/setup_resources.gd
@@ -0,0 +1,20 @@
+extends Node
+
+@export var resources: Dictionary[String, int] = {
+	"Electricity": 0,
+	"Water": 0,
+	"Food": 0,
+	"Waste": 0,
+	"Soil": 0,
+	"Happiness": 0,
+	"Materials": 0,
+	"Seeds": 0,
+}
+
+func _ready() -> void:
+	await get_tree().root.ready
+	give_resources()
+
+func give_resources():
+	for resource in resources:
+		EnvironmentManager.gain_resource(resource, resources[resource])
--- test/scenarios/test_click_to_move/click_to_move.gd
+++ test/scenarios/test_click_to_move/click_to_move.gd
@@ -4,21 +4,26 @@ extends Node3D
 @onready var navigation_agent_3d: NavigationAgent3D = %NavigationAgent3D
 
 @export var speed: float = 10
+@export var marker_animation_time: float = 0.5
 
 var actor: CharacterBody3D
 
 @onready var click_marker: MeshInstance3D = MeshInstance3D.new()
 
+
 func _ready() -> void:
-	#get_parent().
 	add_child(click_marker)
 	click_marker.top_level = true
 	click_marker.mesh = SphereMesh.new()
 	actor = owner
+	click_marker.hide()
 
-#func _physics_process(delta: float) -> void:
-	#move_to_point(delta, speed)
-	#pass
+func animate_click_marker():
+	click_marker.scale = Vector3(1, 1, 1)
+	click_marker.show()
+	var tween := create_tween()
+	tween.tween_property(click_marker, "scale", Vector3(0.1, 0.1, 0.1), marker_animation_time)
+	tween.finished.connect(click_marker.hide)
 
 func get_velocity(delta: float, speed):
 	var target_position = navigation_agent_3d.get_next_path_position()
@@ -28,17 +33,6 @@ func get_velocity(delta: float, speed):
 	if not navigation_agent_3d.is_target_reachable():
 		return Vector3.ZERO
 	return direction * speed
-
-#func calculate_velocity():
-	#var target_position = navigation_agent_3d.get_next_path_position()
-	#var direction = global_position.direction_to(target_position)
-	#actor.velocity = direction * speed
-	#
-#func move_to_point(delta, speed):
-	#var target_position = navigation_agent_3d.get_next_path_position()
-	#var direction = global_position.direction_to(target_position)
-	#actor.velocity = direction * speed
-	#actor.move_and_slide()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("Left_Click"):
@@ -52,8 +46,8 @@ func _unhandled_input(event: InputEvent) -> void:
 		ray_query.from = from
 		ray_query.to = to
 		ray_query.collision_mask
-		#ray_query.collide_with_areas = true
 		var result = space.intersect_ray(ray_query)
 		if result:
 			navigation_agent_3d.target_position = result.position
 			click_marker.global_position = result.position
+			animate_click_marker()
