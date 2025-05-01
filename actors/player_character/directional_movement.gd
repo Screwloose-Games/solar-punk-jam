a9d89c4d05164c8f2af8b862071d448ecb1ad011
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2025-04-30 18:19:18 -0700

    Refactor directional movement

--- actors/player_character/directional_movement.gd
+++ actors/player_character/directional_movement.gd
@@ -1,31 +1,50 @@
 class_name DirectionalMovement
 extends Node
 
+enum MovementBasis {
+	CAMERA,
+	WORLD,
+}
+
+@export var movement_basis_type: MovementBasis = MovementBasis.CAMERA:
+	set(val):
+		movement_basis_type = val
+		if movement_basis_type == MovementBasis.CAMERA:
+			basis_node = _camera
+		else:
+			basis_node = owner
+
 @onready var model: Node3D = %Model
 
 @onready var _camera: PhantomCamera3D = %PlayerPhantomCamera3D
 
-
+var basis_node: Node3D = owner
 var actor: Player
 
 func _ready() -> void:
+	movement_basis_type = movement_basis_type
 	actor = owner
+
+func get_movement_direction(input_direction: Vector2):
+	var movement_direction: Vector3 = Vector3(input_direction.x, 0, input_direction.y).normalized()
+	var final_direction = Vector3.ZERO
+	if movement_direction:
+		var transform_basis : Basis = basis_node.global_transform.basis
+		var horizontal_basis = transform_basis.orthonormalized().get_rotation_quaternion().get_euler().y
+		var rotation_basis = Basis(Vector3.UP, horizontal_basis)
+		movement_direction = rotation_basis * movement_direction.normalized()
+		final_direction.x = movement_direction.x
+		final_direction.z = movement_direction.z
+	return final_direction
 
 func get_velocity_third_person(delta: float, speed: float):
 	var velocity: Vector3 = Vector3.ZERO
 	var input_dir := Input.get_vector("Move_Left", "Move_Right", "Move_Forward", "Move_Backward")
-	var direction: Vector3 = Vector3(input_dir.x, 0, input_dir.y).normalized()
+	var movement_direction = get_movement_direction(input_dir)
 	
-	if direction:
-		var camera_basis : Basis = _camera.global_transform.basis
-		var horizontal_basis = camera_basis.orthonormalized().get_rotation_quaternion().get_euler().y
-		var rotation_basis = Basis(Vector3.UP, horizontal_basis)
-		direction = rotation_basis * direction.normalized()
-		var move_dir: Vector3 = Vector3.ZERO
-		move_dir.x = direction.x
-		move_dir.z = direction.z
-		velocity.x = move_dir.x * actor.speed
-		velocity.z = move_dir.z * actor.speed
+	if movement_direction:
+		velocity.x = movement_direction.x * actor.speed
+		velocity.z = movement_direction.z * actor.speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, actor.speed)
 		velocity.z = move_toward(velocity.z, 0, actor.speed)
@@ -35,6 +54,7 @@ func get_velocity(delta: float, speed: float):
 	return get_velocity_third_person(delta, speed)
 	var velocity: Vector3 = Vector3.ZERO
 	var input_dir := Input.get_vector("Move_Left", "Move_Right", "Move_Forward", "Move_Backward")
+
 	var direction := (actor.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * speed
