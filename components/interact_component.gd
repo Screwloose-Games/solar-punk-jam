23a07cc5d791d2138a28f5bb3ae3cfb515a98210
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2025-04-30 21:41:46 -0700

    Improve build interaction

--- components/interact_component.gd
+++ components/interact_component.gd
@@ -1,10 +1,12 @@
 extends Area3D
 class_name InteractArea3D
 
+@export var interact_action: String
+
 var tracked_nodes: Array[InteractableArea3D] = []
 var selected: InteractableArea3D = null
 
-var player
+var player: Player
 
 
 func _ready():
@@ -17,11 +19,8 @@ func _ready():
 
 func _on_area_entered(area: Area3D):
 	if area is InteractableArea3D:
-		for tracked in tracked_nodes:
-			tracked.unselect()
 		tracked_nodes.append(area)
-		area.select()
-		selected = area
+		select_closest()
 
 
 func select_closest():
@@ -37,8 +36,7 @@ func _on_area_exited(area: Area3D):
 		area.unselect()
 		tracked_nodes.erase(area)
 		if tracked_nodes.size() > 0:
-			selected = tracked_nodes[0]
-			selected.select()
+			select_closest()
 		else:
 			selected = null
 
@@ -47,3 +45,17 @@ func _unhandled_input(event):
 	if event.is_action_pressed("Interact"):
 		if selected is InteractableArea3D:
 			selected.interact(player)
+
+func _get_property_list() -> Array:
+	var props = []
+	var actions = InputMap.get_actions()
+	actions.sort()
+
+	props.append({
+		"name": "interact_action",
+		"type": TYPE_STRING,
+		"hint": PROPERTY_HINT_ENUM,
+		"hint_string": ",".join(actions),
+		"usage": PROPERTY_USAGE_DEFAULT
+	})
+	return props
