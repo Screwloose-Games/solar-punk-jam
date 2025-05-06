444889b8a42b713af4907a62d26e53e3628074c8
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2025-05-05 17:11:58 -0700

    Update start sequence

--- scenes/start_sequence.gd
+++ scenes/start_sequence.gd
@@ -2,32 +2,32 @@ extends Control
 
 signal next
 
-@onready var margin_container: MarginContainer = %MarginContainer
 @onready var continue_button: Button = %ContinueButton
+@onready var label: Label = %Label
+
+@export_multiline var text_panels: PackedStringArray = []
 var next_scene: PackedScene = SceneManager.MAIN_LEVEL
 
+var current_index: int = 0
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	continue_button.pressed.connect(_on_continue_button_pressed)
 	continue_button.grab_focus()
 	continue_button.grab_click_focus()
-	var prompts: Array[Node] = margin_container.get_children()
-	var current_i = 0
-	for i in prompts.size():
-		prompts[i].show()
-		await next
-		prompts[i].hide()
-	SceneTransitionManager.change_scene_with_transition(next_scene, SceneManager.FADE_TRANSITION)
+	_show_next_text()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("Interact"):
 		next.emit()
 
-func _on_continue_button_pressed():
+func _on_continue_button_pressed() -> void:
 	next.emit()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _show_next_text():
+	if current_index < text_panels.size():
+		label.text = text_panels[current_index]
+		await next
+		current_index += 1
+		_show_next_text()
+	else:
+		SceneTransitionManager.change_scene_with_transition(next_scene, SceneManager.FADE_TRANSITION)
