42275da51810aaecb779b30f30d0ebd699b9af34
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2025-05-05 21:27:38 -0700

    Add signals

--- globals/global_signal_bus.gd
+++ globals/global_signal_bus.gd
@@ -11,3 +11,13 @@ signal game_unpaused
 signal win_game_requirements_met
 signal level_reset
 signal set_ending_visibility(show_panel: bool)
+
+# quest-related
+
+signal community_board_interacted
+signal community_board_quest_accepted
+signal talked_to(npc: String)
+signal activated_build_mode
+signal structure_interacted(structure) # Structure ID
+signal seeds_planted(crop: Crop)
+signal player_entered_home
