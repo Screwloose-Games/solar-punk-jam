ee44247e8ebed0b7976a8e7b1e94ebe8e0c41057
loteque <loteque@bluebox.local>
2025-05-07 13:06:18 -0700

    add test scene, remove wav, add ogg

--- components/dynamic_music_box/d_music_box.gd
+++ components/dynamic_music_box/d_music_box.gd
@@ -1,17 +1,25 @@
 extends Area3D
+class_name DynamicMusicRegion
+
     
 @export var region_id: String
 @export var track_idx: int 
+
     
 func _ready():
+    body_entered.connect(_on_body_entered)
+    body_exited.connect(_on_body_exited)
     DynamicMusic.track_region_entered.connect(_on_track_region_entered)
     DynamicMusic.track_region_exited.connect(_on_track_region_exited)
-
-
+    
+    DynamicMusic.play()
+    DynamicMusic.stop_stream(1)
+    print("DynamicMusicRegion ready")
 
 func _on_body_exited(body:Node3D) -> void:
     if body.name == "Player":
         DynamicMusic.track_region_exited.emit(track_idx)
+
 
 func _on_body_entered(body:Node3D) -> void:
     if body.name == "Player":
---
+++ test/scenarios/test_dynamic_music/d_music_test_level.gd
@@ -0,0 +1,4 @@
+extends Node3D
+
+func _ready():
+    get_tree().set_debug_collisions_hint(true)
