654a0ca1182910d01e17b24407f86fcae231fc6d
loteque <loteque@bluebox.local>
2025-05-08 10:38:35 -0700

    add initial test, add SoundPLayer3D

---
+++ test/scenarios/test_sound_player_3d/sp3d_test_level.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+@export var sound_player_3d: AudioStreamPlayer3D
+
+func _ready():
+    
+    var callable = Callable(sound_player_3d, "_on_start")
+    print("Object Instance: ", SoundPlayer3D)
+    print("callable: ", callable)
+    print("signal name: ", sound_player_3d.g_start_signal_name)
+    print("has_method: ", sound_player_3d.has_method("_on_start"))
+    print("try_connect_g_start_esult: ", sound_player_3d.try_connect_g_start_result)
+    print("is_connected: ", sound_player_3d.global_signal_bus.is_connected(sound_player_3d.g_start_signal_name, callable))
+    assert(sound_player_3d.global_signal_bus.is_connected(sound_player_3d.g_start_signal_name, callable))
