0052af0f0d7f2d87cd21c19df9e8da6cc57fa592
jvaikun <jeevan.vaikun@gmail.com>
2025-05-08 13:39:06 +0700

    QuestManager revision and quest update Added functions to QuestManager to process signals from StructureManager and EnvManager Updated quests with new QuestObjective subtypes

--- globals/quest_manager.gd
+++ globals/quest_manager.gd
@@ -26,19 +26,16 @@ var quests : Array[Quest] = []
 signal quest_started(quest_id : String)
 signal quests_changed
 signal quest_completed(giver : String)
+signal structure_built(name : String)
+signal crop_planted(name : String)
 
 
 func _ready() -> void:
 	Dialogic.VAR.variable_changed.connect(check_quests)
+	# Signals that need to be 'digested' to simplify the argument they pass
 	EnvironmentManager.UpdatedAvailableResources.connect(update_resources)
 	StructureManager.StructureBuilt.connect(update_structures)
-	# Global bus toggles
-	GlobalSignalBus.talked_to.connect(update_talk)
 	GlobalSignalBus.seed_planted.connect(update_crop)
-	GlobalSignalBus.community_board_interacted.connect(update_misc.bind("board_met"))
-	GlobalSignalBus.activated_build_mode.connect(update_misc.bind("entered_build"))
-	GlobalSignalBus.seed_ui_shown.connect(update_misc.bind("entered_plant"))
-	GlobalSignalBus.player_entered_home.connect(update_misc.bind("player_entered_home"))
 
 
 func start_quest(file_name : String):
@@ -63,6 +60,15 @@ func update_structures(new_structure):
 		var success = Dialogic.VAR.set_variable(structure_name, true)
 		if not success:
 			push_error("Tried setting non-existant variable")
+	structure_built.emit(StructureManager.get_structure_name(new_structure.structure))
+	check_quests()
+
+
+func update_crop(crop : Crop):
+	match crop.name:
+		"Radish":
+			Dialogic.VAR.crop_radish += 1
+	crop_planted.emit(crop.name)
 	check_quests()
 
 
@@ -73,23 +79,6 @@ func update_resources():
 			if res_varname in Dialogic.VAR.variables():
 				var res_value = EnvironmentManager.current_resources[res_name]
 				Dialogic.VAR.set_variable(res_varname, res_value)
-	check_quests()
-
-
-func update_crop(crop : Crop):
-	match crop.type:
-		Crop.CropType.RADISH:
-			Dialogic.VAR.crop_radish += 1
-	check_quests()
-
-
-func update_talk(npc_id : String):
-	Dialogic.VAR[npc_id + "_met"] = true
-	check_quests()
-
-
-func update_misc(flag : String):
-	Dialogic.VAR[flag] = true
 	check_quests()
 
 
