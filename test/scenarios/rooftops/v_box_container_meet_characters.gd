7acc827858c04919dc57c3fb2d7565a1356a2af3
Josep Valls <josep@valls.name>
2025-05-08 23:08:55 -0700

    some progress

--- test/scenarios/rooftops/v_box_container_meet_characters.gd
+++ test/scenarios/rooftops/v_box_container_meet_characters.gd
@@ -1,25 +1,33 @@
 extends VBoxContainer
 
 func _ready() -> void:
-	for i in EnvironmentManager.characters:
-		var b = Button.new()
-		b.connect("pressed", StructureManager.register_character_structures.bind(i))
-		b.text = "Unlock " + i
-		add_child(b)
-	for i in EnvironmentManager.resources:
-		var b = Button.new()
-		b.connect("pressed", EnvironmentManager.gain_resource.bind(i, 10))
-		b.text = "Gain " + i
-		add_child(b)
-	
-	var d = Button.new()
-	d.connect("pressed", EnvironmentManager.deposit_resource.bind("Food", 1))
-	d.text = "Donate 1 food"
-	add_child(d)
+	if false:
+		for i in EnvironmentManager.characters:
+			var b = Button.new()
+			b.connect("pressed", StructureManager.register_character_structures.bind(i))
+			b.text = "Unlock " + i
+			add_child(b)
+	if true:
+		for i in EnvironmentManager.resources:
+			var b = Button.new()
+			b.connect("pressed", EnvironmentManager.gain_resource.bind(i, 10))
+			b.text = "Gain " + i
+			add_child(b)
+	if false:
+		var d = Button.new()
+		d.connect("pressed", EnvironmentManager.deposit_resource.bind("Food", 1))
+		d.text = "Donate 1 food"
+		add_child(d)
 
-	var b = Button.new()
-	b.connect("pressed", EnvironmentManager.end_day)
-	b.text = "Go to bed"
-	add_child(b)
+		var b = Button.new()
+		b.connect("pressed", EnvironmentManager.end_day)
+		b.text = "Go to bed"
+		add_child(b)
+	if true:
+		var b = Button.new()
+		b.connect("pressed", StructureManager.register_all_structures)
+		b.text = "Unlock everything"
+		add_child(b)
+
 		
 	
