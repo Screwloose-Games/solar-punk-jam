ebc8abf5c519241ca59aca0edd4fedf555afe798
loteque <loteque@bluebox.local>
2025-05-09 16:15:18 -0700

    add patch to dynamic mus sys support more than 2 tracks

--- components/dynamic_music_box/d_music_box.gd
+++ components/dynamic_music_box/d_music_box.gd
@@ -13,7 +13,14 @@ func _ready():
     DynamicMusic.track_region_exited.connect(_on_track_region_exited)
     
     DynamicMusic.play()
-    DynamicMusic.stop_stream(1)
+    
+    var d_stream_count: int = DynamicMusic.stream.stream_count  
+    
+    for stream_idx in d_stream_count:
+        if stream_idx == 0:
+            continue
+        DynamicMusic.stop_stream(stream_idx)
+        
     print("DynamicMusicRegion ready")
 
 func _on_body_exited(body:Node3D) -> void:
