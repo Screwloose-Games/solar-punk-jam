b022ce7d24776b8076c8df70e99c2c16207f1adb
jvaikun <jeevan.vaikun@gmail.com>
2025-05-10 12:44:07 +0700

    UI and quest updates Added styling to Dialogic text boxes Added styling to quest tracker Removed cut characters from Dialogic Quest tracker will now be hidden when current quests are completed

--- common/ui/components/quest/quest_list.gd
+++ common/ui/components/quest/quest_list.gd
@@ -5,14 +5,19 @@ const QUEST_CONTAINER_RES = preload("res://common/ui/components/quest/quest_cont
 
 func _ready() -> void:
 	QuestManager.quests_changed.connect(update_quests)
+	hide()
 
 
 func update_quests():
 	var current_quests = $Content/Body.get_children()
+	var vis_check = false
 	for i in QuestManager.quests.size():
 		if i >= current_quests.size():
 			var new_cont = QUEST_CONTAINER_RES.instantiate()
 			$Content/Body.add_child(new_cont)
 			new_cont.quest = QuestManager.quests[i]
+			vis_check = true
 		elif current_quests[i].quest == QuestManager.quests[i]:
 			current_quests[i].update()
+			vis_check = !QuestManager.quests[i].is_complete
+	visible = vis_check
