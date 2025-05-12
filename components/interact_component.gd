f2f1b473498588b01c32fe6b4be2175f18da4361
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2025-05-11 22:50:26 -0700

    Fix B not working to stop building

--- components/interact_component.gd
+++ components/interact_component.gd
@@ -62,10 +62,10 @@ func _on_area_exited(area: Area3D):
 
 func _unhandled_input(event):
 	if event.is_action_pressed(interact_action):
-		if owner.should_ignore_input:
-			return
 		if owner.is_interacting and selected and selected.toggleable:
 			is_interacting = false
+		if owner.should_ignore_interact:
+			return
 		elif selected is InteractableArea3D:
 			selected.interact(player)
 			selected.stopped_interacting.connect(_on_stopped_interacting.bind(selected))
