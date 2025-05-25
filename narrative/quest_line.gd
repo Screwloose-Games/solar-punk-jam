99747b19e7e05c98451c7c1c50292780291834a5
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2025-05-25 13:04:16 -0700

    Add temp

---
+++ narrative/quest_line.gd
@@ -0,0 +1,29 @@
+extends Resource
+class_name QuestLine
+## A queue of quests. When one is completed, the next one immediately becomes active.
+
+signal completed
+signal quest_completed(quest: Quest)
+signal quest_started(quest: Quest)
+
+@export var name: String
+## The name of the quest line. Breif, unique, human readable
+
+@export var quests: Array[Quest]
+## The quests in sequential order
+
+var active_quest: Quest
+## The current, active quest
+
+func start():
+	start_next_quest()
+
+func start_next_quest():
+	var next_quest: Quest = quests.pop_front()
+	if next_quest is Quest:
+		next_quest.start_quest()
+	else:
+		complete()
+
+func complete():
+	pass
---
+++ test/scenarios/test_water/make_rain.gd
@@ -0,0 +1,19 @@
+extends Node
+
+@onready var environment: CapybaraEnvironment = %Environment
+@onready var animation_player_2: AnimationPlayer = %AnimationPlayer2
+
+@export var next_scene: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	await get_tree().process_frame
+	environment.is_raining = true
+	await get_tree().create_timer(2).timeout
+	animation_player_2.play("panright")
+	#await get_tree().create_timer(4).timeout
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Interact"):
+		SceneTransitionManager.change_scene_with_transition(next_scene, SceneManager.FADE_TRANSITION)
+		
