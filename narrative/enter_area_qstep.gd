8cc024e9644c790378181330144c82f77fe99b1c
jvaikun <jeevan.vaikun@gmail.com>
2025-06-12 15:32:43 +0700

    Moved logic out of QuestTriggerArea to QuestStep

--- narrative/enter_area_qstep.gd
+++ narrative/enter_area_qstep.gd
@@ -1,7 +1,10 @@
-class_name EnterAreaQuestStep 
 extends SignalQuestStep
+class_name EnterAreaQuestStep 
 
-@export var area_id : String
+@export var area_id : String = "area_id"
+@export var start_time_hour : int = 0
+@export var end_time_hour : int = 0
+@export var margin_of_error_minutes : int = 0
 
 
 func set_active(val : bool):
@@ -9,3 +12,13 @@ func set_active(val : bool):
 	signal_name = "quest_trigger_area_entered"
 	expected_args = [area_id]
 	super.set_active(val)
+
+
+func event_occured():
+	var game_time = EnvironmentManager.environment_model.get_in_game_time()
+	if start_time_hour == end_time_hour:
+		super.event_occured()
+	elif game_time.hour >= start_time_hour:
+		if (game_time.hour < end_time_hour) or \
+		(game_time.hour == end_time_hour and game_time.minute <= margin_of_error_minutes):
+			super.event_occured()
--- narrative/quest_trigger_area.gd
+++ narrative/quest_trigger_area.gd
@@ -1,17 +1,8 @@
 extends Area3D
 
 @export var id : String = "area_id"
-@export var margin_of_error_minutes : int = 0
-@export var start_time_hour : int = 0
-@export var end_time_hour : int = 0
 
 
 func _on_body_entered(body: Node3D) -> void:
-	var game_time = EnvironmentManager.environment_model.get_in_game_time()
 	if body is Player:
-		if start_time_hour == end_time_hour:
-			GlobalSignalBus.quest_trigger_area_entered.emit(id)
-		elif game_time.hour >= start_time_hour:
-			if (game_time.hour < end_time_hour) or \
-			(game_time.hour == end_time_hour and game_time.minute <= margin_of_error_minutes):
-				GlobalSignalBus.quest_trigger_area_entered.emit(id)
+		GlobalSignalBus.quest_trigger_area_entered.emit(id)
