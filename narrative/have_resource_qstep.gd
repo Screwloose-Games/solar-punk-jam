08a9b9180acfcad707aca64344def9b8e41ada5d
jvaikun <jeevan.vaikun@gmail.com>
2025-06-13 11:31:26 +0700

    Revised and added signals for QuestSteps Added new signal to GlobalSignalBus for new QuestSteps Added built_structure signal to StructureManager Revised QuestStep subclasses to match signal names

--- narrative/have_resource_qstep.gd
+++ narrative/have_resource_qstep.gd
@@ -8,13 +8,10 @@ func check_value():
 	if is_active:
 		var check_value = ResourcesManager.get_resource_by_name(resource_type)
 		print("Objective check val: " + str(check_value))
-		if typeof(check_value) not in [TYPE_BOOL, TYPE_INT]:
-			print("Value is not int or bool, aborting check.")
+		if int(check_value) >= goal:
+			progress = goal
+			is_completed = true
+			is_active = false
 		else:
-			if int(check_value) >= goal:
-				progress = goal
-				is_completed = true
-				is_active = false
-			else:
-				progress = int(check_value)
-				progressed.emit()
+			progress = int(check_value)
+			progressed.emit()
--- narrative/interact_struct_qstep.gd
+++ narrative/interact_struct_qstep.gd
@@ -1,11 +1,11 @@
 class_name InteractStructureQuestStep
 extends SignalQuestStep
 
-@export var structure_type : String
+@export var structure_name : String
 
 
 func set_active(val : bool):
 	autoload_name = "GlobalSignalBus"
 	signal_name = "structure_interacted"
-	expected_args = [structure_type]
+	expected_args = [structure_name]
 	super.set_active(val)
