556eb422976f51a76016faf1866f5a3c47ea0007
fishiflor <56325674+fishiflor@users.noreply.github.com>
2025-06-13 17:27:17 -0700

    Quest Additions and small community hub modifications
    Move community board quest marker, placed waste bin model in hub, updated harvest quest and added temp unlock quest.

--- assets/3d/structures/community_waste_bin/waste_bin.gd
+++ assets/3d/structures/community_waste_bin/waste_bin.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 	interactable_area_3d.interacted.connect(_on_interacted)
 
 func _on_interacted(_player: Player) -> void:
-	if ResourcesManager.has_at_least("Food", 1):
+	if ResourcesManager.has_at_least(ResourcesManager.ResourceType.FOOD, 1):
 		ResourcesManager.spend_resource(ResourcesManager.ResourceType.FOOD, 1)
 		GlobalSignalBus.food_donated.emit(1)
 		#ResourcesManager.gain_resource_enum(ResourcesManager.ResourceType.COMMUNITY, community_per_donation)
